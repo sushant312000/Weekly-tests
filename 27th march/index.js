ebe7394660f7db4ad2bd276333a09c2353fb4585
Sushant Kumar <suskr.2000@gmail.com>
2022-03-27 11:45:40 +0530

    27th march test

---
+++ 27th march/index.js
@@ -0,0 +1,27 @@
+let library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }
+ ]
+
+ for(let i = 0; i<library.length; i++){
+     if (library[i].readingStatus) {
+         console.log(`Already read ${library[i].title} by ${library[i].author}`);
+     }
+     else{
+        console.log(`read ${library[i].title} by ${library[i].author}`);
+     }
+}
+ 
